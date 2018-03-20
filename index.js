2659a9787176579e99a907f4052826a1dfa7c00e
LennyStrideR <kil.oil@yandex.ru>
2018-03-20 14:59:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ bot.on("ready", () => {
   console.log(`{bot.user.username} is online!`);
 });
 
-bot.login(process.env.NDI1MzQ3ODgyODA0MzE0MTMy.DZImoQ.1vlCB0_Gj7KP4KKfVLHLMx_FpiA);
+client.login(process.env.NDI1MzQ3ODgyODA0MzE0MTMy.DZImoQ.1vlCB0_Gj7KP4KKfVLHLMx_FpiA);
