ed25b54dd054ec1034782e40e2c233e579bf57bd
LennyStrideR <kil.oil@yandex.ru>
2018-03-21 18:43:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 //
+
 const Discord = require("discord.js");
+
 const client = new Discord.Client();
 
+
+
 client.on('ready', () => {
+
     console.log(`Logged in as ${client.user.tag}!`);
+
 });
 
+
+
 client.on('message', msg => {
+
     if (msg.content === 'ping') {
+
     msg.reply('pong');
+
 }
 
+
+
 if (msg.content === 'FN!Flimeus') {
+
     msg.channel.send({
-        embed: {
-            color: 0xff0000,
-            title: "Полезные ссылки",
-            fields: [
-                {
-                    name: 'Твич Флейма',
-                    value: 'https://www.twitch.tv/flimeus'
-                },
-                {
-                    name: 'Ютуб Флейма',
-                    value: 'https://www.youtube.com/channel/UCp_yHWYMIXXQ8LFDdM5Q0SQ'
-                }
-            ],
-            timestamp: new Date(),
+const embed = new Discord.RichEmbed()
+.setColor (#ff0000)
+.setTitle (`**Полезные ссылки`)
+.addField (`FN!Flimeus`)
+.setThumbnail (`https://media.discordapp.net/attachments/418113340871540746/426018657009926144/unknown.png`)
+.setTimestap (new Date())
+
         }
+
     });
+
 }
+
+
 
 });
 
