bf15cc0816dc1f398487c6252a159725365eb1ad
LennyStrideR <kil.oil@yandex.ru>
2018-03-21 19:43:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+let prefix = `FN!`
 
 client.on('ready', () => {
+
+
 
     console.log(`Logged in as ${client.user.tag}!`);
 
@@ -13,7 +16,9 @@ client.on('ready', () => {
 
 
 
-client.on('message', msg => {
+client.on('message', message => {
+
+  let sender = message.author;
 
     if (message.content === 'ping') {
 
@@ -25,21 +30,16 @@ client.on('message', msg => {
 if (message.content === 'FN!Flimeus') {
 
 const embed = new Discord.RichEmbed()
-.setColor (#ff0000)
+.setColor (0xff0000)
 .setTitle (`**Полезные ссылки`)
 .addField (`FN!Flimeus`)
 .setThumbnail (`https://media.discordapp.net/attachments/418113340871540746/426018657009926144/unknown.png`)
-.setTimestap (new Date())
+.setTimestamp (new Date())
 
 message.channel.send({embed});
 
-console.log(`${sender.tag} написал комманду ${prefix}Flimeus`);
-
         }
 
-    });
-
-}
 
 
 
