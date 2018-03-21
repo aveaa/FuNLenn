2295bd54dd1a9db0e06053b3618ebb707ce7de0e
LennyStrideR <kil.oil@yandex.ru>
2018-03-21 18:58:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
-
+if (msg.content === 'FN!Flimeus') {
 
 client.on('ready', () => {
 
@@ -24,15 +24,17 @@ client.on('message', msg => {
 
 
 
-if (msg.content === 'FN!Flimeus') {
 
-    msg.channel.send({
 const embed = new Discord.RichEmbed()
 .setColor (#ff0000)
 .setTitle (`**Полезные ссылки`)
 .addField (`FN!Flimeus`)
 .setThumbnail (`https://media.discordapp.net/attachments/418113340871540746/426018657009926144/unknown.png`)
 .setTimestap (new Date())
+
+message.channel.send({embed});
+
+console.log(`${sender.tag} написал комманду ${prefix}Flimeus`);
 
         }
 
